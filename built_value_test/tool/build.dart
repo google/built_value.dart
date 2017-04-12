a523cac19a601b28d3f3d4b05c888d7b7006f2ca
David Morgan <davidmorgan@google.com>
2017-04-12 14:28:17 +0200

    Add "built_value_test" library with matcher.

---
+++ built_value_test/tool/build.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2017, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:build_runner/build_runner.dart';
+import 'package:built_value_generator/built_value_generator.dart';
+import 'package:source_gen/source_gen.dart';
+
+Future main(List<String> args) async {
+  await build(
+      new PhaseGroup.singleAction(
+          new GeneratorBuilder([new BuiltValueGenerator()]),
+          new InputSet('built_value_test', const ['test/*.dart'])),
+      deleteFilesByDefault: true);
+}
