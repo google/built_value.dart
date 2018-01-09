a8ea43558f41ae247143d1c16bef40a7a05cea60
David Morgan <davidmorgan@google.com>
2018-01-09 14:51:51 +0100

    Add header to error_matchers.dart

--- end_to_end_test/lib/errors_matchers.dart
+++ end_to_end_test/lib/errors_matchers.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2018, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:test/test.dart';
 
 Matcher isErrorContaining(String string) => new _ErrorContaining(string);
