65d9969de3ef65c4649957a3c16545a09ac4bf8d
David Morgan <davidmorgan@google.com>
2020-07-03 13:44:27 +0200

    Update tool/presubmit, run it (formats files).

--- built_value/test/built_list_async_deserializer_test.dart
+++ built_value/test/built_list_async_deserializer_test.dart
@@ -2,7 +2,6 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/async_serializer.dart';
 import 'package:built_value/serializer.dart';
--- built_value/test/int64_serializer_test.dart
+++ built_value/test/int64_serializer_test.dart
@@ -2,7 +2,6 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-
 import 'package:built_value/serializer.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:test/test.dart';
