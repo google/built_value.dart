4692061f06409218c5b571873327b4cecd3c8853
David Morgan <davidmorgan@google.com>
2020-07-03 14:58:48 +0200

    Enable NNBD for end_to_end_test, opt out in all srcs.

--- end_to_end_test/test/imported_values_serializer_test.dart
+++ end_to_end_test/test/imported_values_serializer_test.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, Google Inc. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+// @dart=2.8
 
 import 'package:built_collection/built_collection.dart';
 import 'package:end_to_end_test/imported_values.dart';
