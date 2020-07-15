ce90553a4fbb783c7daf2c41542398c7761aae75
David Morgan <davidmorgan@google.com>
2020-07-15 14:22:29 +0200

    Add null safe versions of polymorphism examples and tests to end to end tests. Fix generator.

---
+++ end_to_end_test/test/polymorphism_nnbd_serializer_test.dart
@@ -0,0 +1,142 @@
+// Copyright (c) 2020, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// @dart=2.9
+
+import 'package:built_collection/built_collection.dart';
+import 'package:end_to_end_test/polymorphism_nnbd.dart';
+import 'package:end_to_end_test/serializers_nnbd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Cat', () {
+    var data = Cat((b) => b
+      ..legs = 4
+      ..tail = true);
+    var serialized = [
+      'Cat',
+      'tail',
+      true,
+      'legs',
+      4,
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+
+    test('can be serialized by moreSerializers', () {
+      expect(moreSerializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized by moreSerializers', () {
+      expect(moreSerializers.deserialize(serialized), data);
+    });
+  });
+
+  group('Robot', () {
+    var data = Robot((b) => b
+      ..legs = 4
+      ..fins = 3);
+    var serialized = [
+      'Robot',
+      'fins',
+      3,
+      'legs',
+      4,
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+  });
+
+  group('StandardCat', () {
+    var data = StandardCat((b) => b..tail = true);
+    var serialized = [
+      'StandardCat',
+      'tail',
+      true,
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+  });
+
+  group('HasField', () {
+    var data = BuiltList<HasField<dynamic>>(<Object>[
+      HasString((b) => b..field = 'hello'),
+      HasDouble((b) => b..field = 3.14)
+    ]);
+    var serialized = [
+      'list',
+      ['HasString', 'field', 'hello'],
+      ['HasDouble', 'field', 3.14]
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+  });
+
+  group('Cage', () {
+    var data = Cage((b) => b
+      ..inhabitant = Cat((b) => b
+        ..tail = true
+        ..legs = 4)
+      ..otherInhabitants.add(Fish((b) => b
+        ..legs = 0
+        ..fins = 4)));
+    var serialized = [
+      'Cage',
+      'inhabitant',
+      ['Cat', 'tail', true, 'legs', 4],
+      'otherInhabitants',
+      [
+        ['Fish', 'fins', 4, 'legs', 0]
+      ],
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+  });
+
+  group('UsesHandCoded', () {
+    var data = UsesHandCoded((b) => b..fieldInBaseBuilder = 4);
+    var serialized = [
+      'UsesHandCoded',
+      'fieldInBaseBuilder',
+      4,
+    ];
+
+    test('can be serialized', () {
+      expect(serializers.serialize(data), serialized);
+    });
+
+    test('can be deserialized', () {
+      expect(serializers.deserialize(serialized), data);
+    });
+  });
+}
---
+++ end_to_end_test/test/polymorphism_nnbd_test.dart
@@ -0,0 +1,148 @@
+// Copyright (c) 2020, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// @dart=2.9
+
+import 'package:end_to_end_test/polymorphism_nnbd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Cat', () {
+    test('can be instantiated', () {
+      Cat((b) => b
+        ..legs = 4
+        ..tail = true);
+    });
+
+    test('has method from mixin', () {
+      expect(
+          Cat((b) => b
+            ..legs = 4
+            ..tail = true).canWalk,
+          true);
+    });
+  });
+
+  group('Fish', () {
+    test('can be instantiated', () {
+      Fish((b) => b
+        ..legs = 0
+        ..fins = 2);
+    });
+
+    test('has method from mixin', () {
+      expect(
+          Fish((b) => b
+            ..legs = 0
+            ..fins = 2).canWalk,
+          false);
+    });
+
+    test('has method from second mixin', () {
+      expect(
+          Fish((b) => b
+            ..legs = 0
+            ..fins = 2).canSwim,
+          true);
+    });
+  });
+
+  group('Robot', () {
+    test('can be instantiated', () {
+      Robot((b) => b
+        ..legs = 0
+        ..fins = 2);
+    });
+
+    test('has method from mixin', () {
+      expect(
+          Robot((b) => b
+            ..legs = 0
+            ..fins = 2).canWalk,
+          false);
+    });
+
+    test('has method from second mixin', () {
+      expect(
+          Robot((b) => b
+            ..legs = 0
+            ..fins = 2).canSwim,
+          true);
+    });
+  });
+
+  group('Animal', () {
+    test('can be used as an interface, including builder', () {
+      final animals = [
+        Cat((b) => b
+          ..legs = 4
+          ..tail = true),
+        Fish((b) => b
+          ..legs = 0
+          ..fins = 2),
+      ];
+
+      final modifiedAnimals = animals
+          .map((animal) => animal.rebuild((b) => b.legs = b.legs! + 1))
+          .toList();
+
+      final expectedAnimals = [
+        Cat((b) => b
+          ..legs = 5
+          ..tail = true),
+        Fish((b) => b
+          ..legs = 1
+          ..fins = 2),
+      ];
+
+      expect(modifiedAnimals, expectedAnimals);
+    });
+  });
+
+  group('Cage', () {
+    test('holds any animal; does not try to use the abstract builder', () {
+      Cage((b) => b.inhabitant = Cat((b) => b
+        ..legs = 4
+        ..tail = true));
+    });
+  });
+
+  group('StandardCat', () {
+    test('uses legs field from mixin', () {
+      expect(StandardCat((b) => b..tail = true).legs, 4);
+    });
+  });
+
+  group('HasField', () {
+    test('can be used as an interface, including builder', () {
+      final hasFields = <HasField<dynamic>>[
+        HasString((b) => b..field = 'hello'),
+        HasDouble((b) => b..field = 3.14)
+      ];
+
+      final modifiedHasFields = hasFields
+          .map((hasField) => hasField.rebuild((b) => b..field += b.field));
+
+      final expectedHasFields = [
+        HasString((b) => b..field = 'hellohello'),
+        HasDouble((b) => b..field = 6.28)
+      ];
+
+      expect(modifiedHasFields, expectedHasFields);
+    });
+  });
+
+  group('UsesHandCoded', () {
+    test('can be instantiated', () {
+      UsesHandCoded((b) => b..fieldInBaseBuilder = 3);
+    });
+
+    test('can be updated via base interface', () {
+      final HandCoded handCoded =
+          UsesHandCoded((b) => b..fieldInBaseBuilder = 3);
+      final updatedHandCoded =
+          handCoded.rebuild((b) => b..fieldInBaseBuilder = 4);
+      expect(updatedHandCoded.fieldInBaseBuilder, 4);
+    });
+  });
+}
