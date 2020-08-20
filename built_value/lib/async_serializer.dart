20989f7d9592bb60f83a89dd1b15a651659744fa
Mike Fairhurst <mfairhurst@google.com>
2020-08-20 11:29:17 -0700

    Remove unused dart:async import
    Since this package does not support Dart 2.0, this import is not
    necessary.

--- built_value/lib/async_serializer.dart
+++ built_value/lib/async_serializer.dart
@@ -2,8 +2,6 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:built_value/serializer.dart';
 
 /// Deserializer for `BuiltList` that runs asynchronously.
