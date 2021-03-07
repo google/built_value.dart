d83116e2838254c4e8a421393f8780cfa8d61c54
David Morgan <davidmorgan@google.com>
2021-03-07 09:30:27 +0100

    Update chat example to webdev.

--- chat_example/lib/server/resource_server.dart
+++ chat_example/lib/server/resource_server.dart
@@ -14,7 +14,7 @@ typedef SocketReceiver = void Function(WebSocketChannel webSocket);
 
 /// Resource server for the built_value chat example.
 ///
-/// Proxies to pub serve for most requests. Accepts web socket connections.
+/// Proxies to webdev for most requests. Accepts web socket connections.
 class ResourceServer {
   /// Serves resources, passing new sockets to [socketReceiver].
   Future start(SocketReceiver socketReceiver) async {
@@ -22,17 +22,17 @@ class ResourceServer {
         // Web socket handler will do nothing for non-websocket requests, so
         // just add it in the cascade at the top.
         .add(webSocketHandler(socketReceiver))
-        // Anything else goes to pub serve.
+        // Anything else goes to webdev.
         .add(proxyHandler(Uri.parse('http://localhost:8080')))
-        // If that didn't work, must be a problem with pub serve.
+        // If that didn't work, must be a problem with webdev.
         .add((_) {
-      print('Request failed. Check pub serve output for errors.');
+      print('Request failed. Check webdev output for errors.');
       return Response.notFound('');
     });
 
     await io.serve(cascade.handler, 'localhost', 26199).then((server) {
       print('Serving at http://${server.address.host}:${server.port}. Please '
-          'also run pub serve on port 8080.');
+          'also run webdev on port 8080.');
     });
   }
 }
